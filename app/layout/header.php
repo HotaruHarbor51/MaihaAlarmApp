12d067d4c70170915e2e87d7b261413b0c463fc3
HotaruHarbor51 <102233603+HotaruHarbor51@users.noreply.github.com>
2022-09-17 16:00:18 +0900

    gitignore、リセットCSS、ヘッダー、フッター、escape処理追加
    escape処理はHTMLタグが入ったときにエスケープしてくれる処理のこと
    jQueryはAjax通信で削除処理をするときに使い、バージョンは最新のものを使います。

---
+++ app/layout/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>色織まいはのお茶会用アラームアプリ</title>
+
+    <link rel="stylesheet" href="/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="/css/reset.css" />
+    <link rel="stylesheet" href="/css/style.css" />
+    <script src="/js/jquery-3.6.1.min.js"></script>
+    <script src="/js/bootstrap.bundle.min.js"></script>
+  </head>
+
+  <body>
+
+  </body>
+</html>
